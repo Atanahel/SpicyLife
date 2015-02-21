c06b6d1e0e17550a55ff12f50f02aa18e35c7793
Lauriane Mollier <mollierlauriane@gmail.com>
2015-02-21 15:08:38 +0100

    Oremiere affichage

--- static/js/display_activity.js
+++ static/js/display_activity.js
@@ -3,9 +3,22 @@
  */
 
 
-function (){
-function display_activity(){
+function display_activity(item){
 
+    var address = "<address> <div>" + item.address + "</div> <div>" + item.zipcode + " " + item.city + "</div>" + "</address>";
+
+    var main_info = "<div>" +
+        "<h4>" + item.name + "</h4>" +
+        "<p>" + item.description+ "</p>" +
+        "<footer>" + item.tags.toString()+ "</footer>" +
+        "</div>";
+
+
+    return "<div class='row well'>" +
+        "<div class='col-sm-2'> <img class='img-thumbnail' src='img/ski.jpg'/> </div>" +
+        "<div class='col-sm-8'>" + main_info + "</div>" +
+        "<div class='col-sm-2'>" + address + "</div>" +
+        "</div>";
 
 
 }
