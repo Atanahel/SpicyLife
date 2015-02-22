f01509db7baf782216529ae9f59059c6446368a6
Damien Buchs <damien.buchs@unil.ch>
2015-02-22 09:27:24 +0100

    no message

--- static/js/display_activity.js
+++ static/js/display_activity.js
@@ -5,7 +5,8 @@
 
 function display_activity(item){
 
-    var address = "<address> <div>" + item.address + "</div> <div>" + item.zipcode + " " + item.city + "</div>" + "</address>";
+    var address = "<p><small>" + item.address + ", " + item.zipcode + " " + item.city + "</small></p>" ;
+    var price = "CHF " + item.price + ".-";
 
     var main_info = "<div>" +
         "<h4>" + item.name + "</h4>" +
@@ -16,9 +17,9 @@ function display_activity(item){
 
 
     return "<div class='row well'>" +
-        "<div class='col-sm-2'> <img class='img-thumbnail' src='img/ski.jpg'/> </div>" +
-        "<div class='col-sm-8'>" + main_info + "</div>" +
-        "<div class='col-sm-2'>" + address + "</div>" +
+        "<div class='col-sm-2 litteMP'> <img class='img-thumbnail' src='img/ski.jpg'/> </div>" +
+        "<div class='col-sm-8 litteMP'>" + main_info + "<br>" + address + "</div>" +
+        "<div class='col-sm-2 litteMP'>" + price + "</div>" +
         "</div>";
 
 
