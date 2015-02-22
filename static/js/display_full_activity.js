f285dffc4bbc4ddc1a4f5b8cf32ca52f65cbfdf0
Lauriane Mollier <mollierlauriane@gmail.com>
2015-02-22 09:34:28 +0100

    new page

---
+++ static/js/display_full_activity.js
@@ -0,0 +1,20 @@
+/**
+ * Created by laurianemollier on 21/02/15.
+ */
+
+function display_full_activity(key){
+
+
+
+
+
+
+    $.getJSON(key)
+        .done(function(data) {
+
+
+
+        });
+
+
+}
