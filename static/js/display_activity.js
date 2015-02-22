9d8659d8559b5e35e751f109cc85b8733f8775bc
Damien Buchs <damien.buchs@unil.ch>
2015-02-22 12:11:04 +0100

    new disgn

--- static/js/display_activity.js
+++ static/js/display_activity.js
@@ -7,19 +7,21 @@ function display_activity(item){
 
     var address = "<p><small>" + item.address + ", " + item.zipcode + " " + item.city + "</small></p>" ;
     var price = "CHF " + item.price + ".-";
+    var distance = "" +  Math.round(item.dist*10)/10 + "";
 
     var main_info = "<div>" +
-        "<h4>" + item.name + "</h4>" +
-        "<p>" + item.description+ "</p>" +
+        item.name +
         "<small style='color: gray;'>" + item.tags.toString()+ "</small>" +
-        // item.price
         "</div>";
 
 
-    return "<div class='row well'>" +
-        "<div class='col-sm-2 litteMP'> <img class='img-thumbnail' src='img/ski.jpg'/> </div>" +
-        "<div class='col-sm-8 litteMP'>" + main_info + "<br>" + address + "</div>" +
-        "<div class='col-sm-2 litteMP'>" + price + "</div>" +
+    return "<div class='col-sm-6 litteMP' >"+
+        "<div class='row well' style='background-image: url(" + item.img_url +") ;'>" +
+        "<div class='pull-right distance'><small>" + distance + "&nbsp;km</small></div>" +
+        "<div class='banner'>" +
+        "<div class='col-sm-8 litteMP'><strong>" + main_info  + "</strong></div>" +
+        "<div class='col-sm-4 litteMP pull-right text-right'><small>" + price + "</small></div>" +
+        "</div>" +
         "</div>";
 
 
